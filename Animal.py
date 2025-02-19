d98001583313ed05862305a60b119ea656e18ba5
Sonyl Nagale <sonylnagale@users.noreply.github.com>
2025-02-18 19:47:26 -0500

    Changed Animal class and usage

--- Animal.py
+++ Animal.py
@@ -1,6 +1,7 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name):
+        self.__name = name 
+        print("hello, I am", self.__name)
 
     def talk(self):
         print("hi")
